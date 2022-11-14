bb3f105de11b43839b780bf23e7c90f5c67cbfc0
XMarcozX <xmarco11x@gmail.com>
2022-11-14 22:39:33 +0700

    Начальная страница (заготовка)

--- app/src/main/java/com/example/kursovayaboletti/MainActivity.kt
+++ app/src/main/java/com/example/kursovayaboletti/MainActivity.kt
@@ -7,6 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //test
+
+        /*
+        Проверяем, разместился ли фрагмент в контейнере. Если нет, то создаем экземпляр
+        GalleryFragment и добавляем его в контейнер
+         */
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, GalleryFragment.newInstance())
+                .commit()
+        }
     }
 }
